0c8c8f243595326da9ee5a28cfce6a1ff8d47baf
Phil Rigovanov <phil@rigovanov.ru>
2017-09-24 20:18:03 +0300

    dirs renamed

--- Chapter-01/Page_021.js
+++ Chapter-01/Page_021.js
@@ -1,11 +1,11 @@
-// Draw as many cats as you want!
+// Рисуем столько котиков, сколько захотим!
 var drawCats = function (howManyTimes) {
   for (var i = 0; i < howManyTimes; i++) {
     console.log(i + " =^.^=");
   }
 };
 
-drawCats(10); // You can put any number here instead of 10.
+drawCats(10); // Вместо 10 тут может быть другое число
 
 // 0 =^.^=
 // 1 =^.^=
