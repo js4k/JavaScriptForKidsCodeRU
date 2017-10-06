72bd8433a83ecb45240adb025e952e97948c0af8
Phil Rigovanov <phil@rigovanov.ru>
2017-10-06 17:24:45 +0300

    untranslated string found

--- Chapter-02/Page-039a.js
+++ Chapter-02/Page-039a.js
@@ -2,6 +2,6 @@ var longString = "Эта длинная строка такая длинная";
 longString.slice(3, 14);
 // "длинная строка"
 
-var longString = "My long string is long";
+var longString = "Эта длинная строка такая длинная";
 longString.slice(3);
 // "длинная строка такая длинная"
